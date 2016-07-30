cda49f70cfbafffc22bb814ccdbeb2cb07bdc3bb
Jack <jackm@aloompa.com>
2016-07-30 02:40:24 -0500

    Updated Kotlin config

--- app/src/main/kotlin/com/malpo/potluck/PotluckApplication.kt
+++ app/src/main/kotlin/com/malpo/potluck/PotluckApplication.kt
@@ -1,9 +1,11 @@
 package com.malpo.potluck
 
 import android.app.Application
+import com.firebase.client.Firebase
 
 class PotluckApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Firebase.setAndroidContext(this)
     }
 }
