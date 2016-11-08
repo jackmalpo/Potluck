735ecda439c822afc7a031a6b4ae978b879a31cb
Jack <jackmalpo@gmail.com>
2016-11-07 22:29:01 -0600

    Converted PreferenceStoreTest to Spek

--- app/src/test/kotlin/com/malpo/potluck/preferences/PreferenceStoreTest.kt
+++ app/src/test/kotlin/com/malpo/potluck/preferences/PreferenceStoreTest.kt
@@ -2,83 +2,76 @@ package com.malpo.potluck.preferences
 
 import android.content.SharedPreferences
 import com.malpo.potluck.models.spotify.Token
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.eq
-import com.nhaarman.mockito_kotlin.verify
-import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.*
 import com.squareup.moshi.Moshi
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import org.junit.Before
 import org.junit.Test
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.reset
 import org.mockito.MockitoAnnotations
 import rx.observers.TestSubscriber
 
-class PreferenceStoreTest {
-
-    @Mock
-    lateinit var mockPrefs: SharedPreferences
-
-    @Mock
-    lateinit var mockEditor: SharedPreferences.Editor
-
-    lateinit var preferenceStore: PreferenceStore
-
+@RunWith(JUnitPlatform::class)
+class PreferenceStoreTest : Spek({
+    val mockPrefs: SharedPreferences = mock()
+    val mockEditor: SharedPreferences.Editor = mock()
     val moshi: Moshi = Moshi.Builder().build()
-
     val sampleToken = Token(accessToken = "123", expiresIn = 1, scope = "Shrug", refreshToken = "456", token_type = "Normal")
-
     val sampleTokenString = moshi.adapter(Token::class.java).toJson(sampleToken)
 
-    @Before
-    fun setup() {
-        MockitoAnnotations.initMocks(this)
+    given("A preference store") {
+        val preferenceStore = PreferenceStore(mockPrefs, moshi)
 
-        whenever(mockEditor.clear()).thenReturn(mockEditor)
-        whenever(mockEditor.putString(any(), any())).thenReturn(mockEditor)
-        whenever(mockPrefs.edit()).thenReturn(mockEditor)
+        beforeEach {
+            reset(mockPrefs, mockEditor)
 
-        preferenceStore = PreferenceStore(mockPrefs, moshi)
+            whenever(mockEditor.clear()).thenReturn(mockEditor)
+            whenever(mockEditor.putString(any(), any())).thenReturn(mockEditor)
+            whenever(mockPrefs.edit()).thenReturn(mockEditor)
+        }
+
+        it("Should put the string in the SPOTIFY_GUEST_TOKEN SharedPreference") {
+            preferenceStore.setSpotifyGuestToken().call(sampleToken)
+            verify(mockEditor).putString(eq(PreferenceStore.Companion.SPOTIFY_GUEST_TOKEN), eq(sampleTokenString))
+        }
+
+        it("Should emit observables of guest token updates") {
+            val ts = TestSubscriber<Token>()
+            whenever(mockPrefs.getString(any(), any())).thenReturn(sampleTokenString)
+            preferenceStore.spotifyGuestToken().subscribe(ts)
+            verify(mockPrefs).getString(eq(PreferenceStore.Companion.SPOTIFY_GUEST_TOKEN), any())
+            ts.assertReceivedOnNext(arrayListOf(sampleToken))
+        }
+
+        it("Should save the host token") {
+            preferenceStore.setSpotifyHostToken().call(sampleToken)
+            verify(mockEditor).putString(eq(PreferenceStore.Companion.SPOTIFY_HOST_TOKEN), eq(sampleTokenString))
+        }
+
+        it("Should save the refresh token when the host token is saved") {
+            preferenceStore.setSpotifyHostToken().call(sampleToken)
+            verify(mockEditor).putString(PreferenceStore.Companion.SPOTIFY_HOST_REFRESH, sampleToken.refreshToken)
+        }
+
+        it("Should emit observables of host token updates") {
+            val ts = TestSubscriber<Token>()
+            whenever(mockPrefs.getString(any(), any())).thenReturn(sampleTokenString)
+            preferenceStore.spotifyHostToken().subscribe(ts)
+            verify(mockPrefs).getString(eq(PreferenceStore.Companion.SPOTIFY_HOST_TOKEN), any())
+            ts.assertReceivedOnNext(arrayListOf(sampleToken))
+        }
+
+        it("Should return the refresh token") {
+            preferenceStore._spotifyHostRefreshToken()
+            verify(mockPrefs).getString(eq(PreferenceStore.Companion.SPOTIFY_HOST_REFRESH), any())
+        }
     }
-
-    @Test
-    fun setSpotifyGuestToken() {
-        preferenceStore.setSpotifyGuestToken().call(sampleToken)
-        verify(mockEditor).putString(eq(PreferenceStore.Companion.SPOTIFY_GUEST_TOKEN), eq(sampleTokenString))
-    }
-
-    @Test
-    fun spotifyGuestToken() {
-        val ts = TestSubscriber<Token>()
-        whenever(mockPrefs.getString(any(), any())).thenReturn(sampleTokenString)
-        preferenceStore.spotifyGuestToken().subscribe(ts)
-        verify(mockPrefs).getString(eq(PreferenceStore.Companion.SPOTIFY_GUEST_TOKEN), any())
-        ts.assertReceivedOnNext(arrayListOf(sampleToken))
-    }
-
-    @Test
-    fun setSpotifyHostToken_savesHostToken() {
-        preferenceStore.setSpotifyHostToken().call(sampleToken)
-        verify(mockEditor).putString(eq(PreferenceStore.Companion.SPOTIFY_HOST_TOKEN), eq(sampleTokenString))
-    }
-
-    @Test
-    fun setSpotifyHostToken_savesRefreshToken() {
-        preferenceStore.setSpotifyHostToken().call(sampleToken)
-        verify(mockEditor).putString(eq(PreferenceStore.Companion.SPOTIFY_HOST_REFRESH), eq(sampleToken.refreshToken))
-    }
-
-    @Test
-    fun spotifyHostToken() {
-        val ts = TestSubscriber<Token>()
-        whenever(mockPrefs.getString(any(), any())).thenReturn(sampleTokenString)
-        preferenceStore.spotifyHostToken().subscribe(ts)
-        verify(mockPrefs).getString(eq(PreferenceStore.Companion.SPOTIFY_HOST_TOKEN), any())
-        ts.assertReceivedOnNext(arrayListOf(sampleToken))
-    }
-
-    @Test
-    fun getSpotifyHostRefreshToken() {
-        preferenceStore._spotifyHostRefreshToken()
-        verify(mockPrefs).getString(eq(PreferenceStore.Companion.SPOTIFY_HOST_REFRESH), any())
-    }
-}
+})
